e5ae49a6f468bf4ac2abe87e25fbe0884b1315ac
Tchougwen01 <tewobrice@gmail.com>
2022-04-26 15:02:01 +0100

    Initial commit

---
+++ src/main/java/com/example/inf3196_groupe12/Inf3196Groupe12Application.java
@@ -0,0 +1,13 @@
+package com.example.inf3196_groupe12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inf3196Groupe12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Inf3196Groupe12Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inf3196_groupe12/Inf3196Groupe12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inf3196_groupe12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inf3196Groupe12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
